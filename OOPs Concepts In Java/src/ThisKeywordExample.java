46023db3278ef7cb919d1a436223a312d22d32bd
Tejpal Choudhary <choudharytejpal148@gmail.com>
2023-02-11 10:55:56 +0530

    This  Keyword example of refercing to the instance variable of current class

--- OOPs Concepts In Java/src/ThisKeywordExample.java
+++ OOPs Concepts In Java/src/ThisKeywordExample.java
@@ -3,9 +3,14 @@ import java.util.Scanner;
 public class ThisKeywordExample {
     int a,b;
     public ThisKeywordExample(int a,int b) {
-        this.a=a;  //here this keyword is referenceing the instance variable of current class
-        this.b=b;
+//        this.a=a;  //here this keyword is referenceing the instance variable of current class
+//        this.b=b; //if we dont use this keyword then local variable and instance variable are the same and and we can't
+        //differienciate between them so we will get 0 ans
 
+//        ******Just try this*******
+            a=a;
+            b=b;
+            //out put will 0,0 always even whatever input we give.
     }
     void show(){
         System.out.println("a is :"+a+" & "+"b is :"+b);
