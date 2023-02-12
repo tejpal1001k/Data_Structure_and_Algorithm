4034e40e6994a57022c7a4aae2640be2f60bef16
Tejpal Choudhary <choudharytejpal148@gmail.com>
2023-02-12 18:10:20 +0530

    EncapsulationExample class

---
+++ OOPs Concepts In Java/src/Class1.java
@@ -0,0 +1,16 @@
+public class Class1 {
+    private int id;
+    private String name;
+    public int getId1(){
+        return id;
+    }
+    public void setId1(int id){
+        this.id=id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName( String name){
+        this.name=name;
+    }
+}
---
+++ OOPs Concepts In Java/src/EncapsulationMainClass.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class EncapsulationMainClass {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        Class1 c=new Class1();
+        System.out.println("Enter Student Id: and Name ");
+//        System.out.println("Enter Student Name: ");
+        c.setId1(sc.nextInt());
+        c.setName(sc.nextLine());
+
+        System.out.println("ID: " + c.getId1());
+        System.out.println("Name: " + c.getName());
+
+    }
+}
